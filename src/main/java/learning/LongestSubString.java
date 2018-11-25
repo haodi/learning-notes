784c36955379174bad19967c851f7e2875f483ae
lihaodi <haodilee@yeah.net>
2018-11-25 16:17:12 +0800

    无重复字符的最长子串

---
+++ src/main/java/learning/LongestSubString.java
@@ -0,0 +1,35 @@
+package learning;
+
+import java.util.TreeSet;
+
+public class LongestSubString {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring(" "));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        String[] characters = s.split("");
+        String subString;
+        TreeSet<Integer> subStringLengths = new TreeSet<>();
+
+        for (int i = 0; i < characters.length; i++) {
+            subString = characters[i];
+
+            for (int j = i + 1; j < characters.length; j++) {
+                if (subString.contains(characters[j])) {
+                    break;
+                } else {
+                    subString += characters[j];
+                }
+            }
+            subStringLengths.add(subString.length());
+        }
+
+        return subStringLengths.size() == 0 ? 0 : subStringLengths.last();
+    }
+}
